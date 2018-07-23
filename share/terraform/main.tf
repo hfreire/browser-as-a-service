db4c6b1c4bf5ba59374a0b332bc0d24aa7d752d6
Hugo Freire <hfreire@exec.sh>
2018-07-23 20:10:11 +0200

    chore(terraform): add support for setting up api keys

--- share/terraform/main.tf
+++ share/terraform/main.tf
@@ -23,6 +23,7 @@ module "browser-as-a-service" {
   name                  = "${var.name}"
   domain_name           = "${var.domain_name}"
   container_definitions = "${data.template_file.container_definitions.rendered}"
+  api_keys              = "${var.api_keys}"
   api_quota_limit       = 1000
   api_quota_offset      = 0
   api_quota_period      = "DAY"
