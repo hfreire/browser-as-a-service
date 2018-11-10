4a6d3262d7be3cbc0db43edf7e6d9b6e45aeb7be
Hugo Freire <hfreire@exec.sh>
2018-11-10 09:32:24 +0100

    docs(copyright): update copyright year

--- test/routes/open.js
+++ test/routes/open.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, Hugo Freire <hugo@exec.sh>.
+ * Copyright (c) 2018, Hugo Freire <hugo@exec.sh>.
  *
  * This source code is licensed under the license found in the
  * LICENSE.md file in the root directory of this source tree.
